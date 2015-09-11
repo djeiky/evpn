b195527e57cdd1838f74ec64e88bb54cae932169
djeik <djeik@inbox.ru>
2015-09-11 13:08:09 +0300

    add devise

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,14 @@ module Evpn
     # config.time_zone = 'Central Time (US & Canada)'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '*.{rb,yml}').to_s]
+    config.assets.enabled = true
+    config.i18n.default_locale = :ru
+    config.i18n.locale = :ru
+    config.i18n.available_locales = [:en, :ru]
+    config.i18n.enforce_available_locales = false
+    config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
+#    I18n.default_locale = :ru
+
   end
 end
